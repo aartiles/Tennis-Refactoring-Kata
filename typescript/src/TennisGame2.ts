d6ed053b58779f9c2ccedc6128267a863e740fbe
Alfredo Artiles <aartiles@gmail.com>
2019-06-28 18:00:35 +0200

    Extract Player class

--- typescript/src/TennisGame2.ts
+++ typescript/src/TennisGame2.ts
@@ -7,34 +7,65 @@ const SCORE_TO_TEXT = {
   3: 'Forty'
 };
 
-export class TennisGame2 implements TennisGame {
-  player1Points: number = 0;
-  player2Points: number = 0;
+class Player {
+  private points: number = 0;
+  private name: string;
 
-  private player1Name: string;
-  private player2Name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  wonPoint() {
+    this.points++;
+  }
+
+  score(): string {
+    return SCORE_TO_TEXT[this.points];
+  }
+
+  isDraw(player: Player): boolean {
+    return this.points < 3 && this.points === player.points;
+  }
+
+  isDeuce(player: Player): boolean {
+    return this.points >= 3 && this.points === player.points;
+  }
+
+  is(name: string): boolean {
+    return name === this.name;
+  }
+
+  difference(player: Player): number {
+    return this.points - player.points;
+  }
+
+  isAdvantage(): boolean {
+    return this.points >= 4;
+  }
+}
+
+export class TennisGame2 implements TennisGame {
+  private player1: Player;
+  private player2: Player;
 
   constructor(player1Name: string, player2Name: string) {
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
+    this.player1 = new Player(player1Name);
+    this.player2 = new Player(player2Name);
   }
 
   getScore(): string {
-    if (this.isDraw()) {
-      return this.drawScore();  
-    }
-    if (this.isAdvantage()) {
-      return this.advantageScore();  
-    }
+    if (this.player1.isDraw(this.player2)) return this.drawScore();  
+    if (this.player1.isDeuce(this.player2)) return this.deuceScore();  
+    if (this.isAdvantage()) return this.advantageScore();  
     return this.runningScore();
   }
 
   private runningScore() {
-    return SCORE_TO_TEXT[this.player1Points] + '-' + SCORE_TO_TEXT[this.player2Points];
+    return `${this.player1.score()}-${this.player2.score()}`;
   }
 
   private advantageScore() {
-    const difference = this.player1Points - this.player2Points;
+    const difference = this.player1.difference(this.player2);
     if (difference >= 2) return  'Win for player1';
     if (difference <= -2) return 'Win for player2';
     if (difference === 1) return 'Advantage player1';
@@ -42,32 +73,25 @@ export class TennisGame2 implements TennisGame {
   }
 
   private isAdvantage() {
-    return this.player1Points >= 4 || this.player2Points >= 4;
+    return this.player1.isAdvantage() || this.player2.isAdvantage();
   }
 
   private drawScore() {
-    if (this.player1Points < 3) {
-      return `${SCORE_TO_TEXT[this.player1Points]}-All`;
-    }
-    else return 'Deuce';
+    return `${this.player1.score()}-All`;
+  }
+
+  private deuceScore() {
+    return 'Deuce';
   }
 
   private isDraw() {
-    return this.player1Points === this.player2Points;
+    return this.player1.isDraw(this.player2);
   }
 
-  P1Score(): void {
-    this.player1Points++;
-  }
-
-  P2Score(): void {
-    this.player2Points++;
-  }
-
-  wonPoint(player: string): void {
-    if (player === this.player1Name)
-      this.P1Score();
+  wonPoint(playerName: string): void {
+    if (this.player1.is(playerName))
+      this.player1.wonPoint();
     else
-      this.P2Score();
+    this.player2.wonPoint();
   }
 }
